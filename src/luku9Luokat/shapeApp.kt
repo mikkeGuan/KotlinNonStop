5e68441f3aea21c251443bf11a9d14b5c1674b7b
Ruichao Guan <mikkeguan@gmail.com>
2024-08-01 10:00:53 +0300

    class exercise 1 done

---
+++ src/luku9Luokat/shapeApp.kt
@@ -0,0 +1,14 @@
+package luku9Luokat
+
+class Shape(xPos: Int = 0, yPos: Int = 0) {
+    var xPos = 0
+    var yPos = 0
+    fun print() {
+        println("Position: x=$xPos, y=$yPos")
+    }
+}
+fun main() {
+    Shape().print()
+
+
+}

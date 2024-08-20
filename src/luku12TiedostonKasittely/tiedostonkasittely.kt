ec5bb983334f722d036ca19569c908df9ac78ff1
Ruichao Guan <mikkeguan@gmail.com>
2024-08-20 23:43:38 +0300

    Chapter 12 file handling exercise 1 done

--- src/luku12TiedostonKasittely/tiedostonkasittely.kt
+++ src/luku12TiedostonKasittely/tiedostonkasittely.kt
@@ -3,18 +3,21 @@ package luku12TiedostonKasittely
 import java.io.File
 
 fun main() {
-    val list = mutableListOf<String>()
     val file = File("readme.txt")
 
+
+
     while (true) {
-        println("Type a line: ")
+        println("Type a line:")
         val word = readLine().toString()
 
         if (word == "exit") {
             break
         }
 
-        list.add(word)
-        file.writeText(list.joinToString(separator = "\n"))
+        file.appendText("$word\n")
     }
+
+    val content = file.readText()
+    println(content)
 }
